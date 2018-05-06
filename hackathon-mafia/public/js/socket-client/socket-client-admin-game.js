581c75d143ce4ba191cf43fd2004b534a2c431b1
Ivo <imladenov1997@gmail.com>
2018-05-06 11:00:12 +0100

    Sounds partially imported

--- hackathon-mafia/public/js/socket-client/socket-client-admin-game.js
+++ hackathon-mafia/public/js/socket-client/socket-client-admin-game.js
@@ -1,3 +1,28 @@
 console.log('admin started game');
 socket.emit("adminStartedGame", {game: gameID});
 
+socket.on('wakeUp', (msg) => {
+    let sound = new Audio('/music/villageWakeUp.mp3');
+    sound.play();
+});
+
+socket.on('fallAsleep', (msg) => {
+    let sound = new Audio('/music/villageFallsAsleep.mp3');
+    sound.play();
+});
+
+socket.on('sheriffTime', (msg) => {
+    let sound = new Audio('/music/seerWakeUp.mp3');
+    sound.play();
+});
+
+socket.on('doctorTime', (msg) => {
+    let sound = new Audio('/music/doctorWakeUp.mp3');
+    sound.play();
+});
+
+socket.on('mafiaTime', (msg) => {
+    let sound = new Audio('/music/warewolvesWakeUp.mp3');
+    sound.play();
+});
+
